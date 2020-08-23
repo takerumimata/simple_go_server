d8b3c5fc2ccab72fc3ca6bd35c7b10437e3fa433
takerumimata <fossil1908@gmail.com>
2020-08-23 17:54:29 +0900

    create request handler sample code

---
+++ reqest_handle_sample.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
